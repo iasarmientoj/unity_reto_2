f35c9f45c0ebcf49e94dc2b2113093f0a3d6d933
novatransmedia <76073483+novatransmedia@users.noreply.github.com>
2023-08-11 21:17:54 -0500

    bala direccion correcta

--- Assets/Scripts/PlayerBala.cs
+++ Assets/Scripts/PlayerBala.cs
@@ -9,12 +9,16 @@ public class PlayerBala : MonoBehaviour
     public Transform origenBala;
 
     public float tiempoBala = 1.5f;
+    public float fuerzaDisparo = 1000f;
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             GameObject nuevaBala = Instantiate(prefabBala, origenBala.position, origenBala.rotation);
+
+
+            nuevaBala.GetComponent<Rigidbody2D>().AddForce(new Vector2(origenBala.lossyScale.x* fuerzaDisparo, 0));
             StartCoroutine(DestruirBala(nuevaBala));
         }
     }
